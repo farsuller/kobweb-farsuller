978e17d8a77d9c4315aeeec0add8d46b0a9cb4f7
farsuller <farsuller2020@gmail.com>
2023-04-13 15:45:40 +0800

    commented not needed on build gradler

--- site/build.gradle.kts
+++ site/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.jetbrains.compose)
     alias(libs.plugins.kobweb.application)
-    alias(libs.plugins.kobwebx.markdown)
+  //  alias(libs.plugins.kobwebx.markdown)
 }
 
 group = "com.far.suller"
@@ -34,8 +34,8 @@ kobweb {
 }
 
 kotlin {
-    configAsKobwebApplication("landing", includeServer = true)
-    jvmToolchain(11) // Kobweb server should use at least Java 11
+    configAsKobwebApplication("landing")
+ //   jvmToolchain(11) // Kobweb server should use at least Java 11
 
     sourceSets {
         val commonMain by getting {
@@ -50,13 +50,13 @@ kotlin {
                 implementation(libs.kobweb.core)
                 implementation(libs.kobweb.silk.core)
                 implementation(libs.kobweb.silk.icons.fa)
-                implementation(libs.kobwebx.markdown)
+             //   implementation(libs.kobwebx.markdown)
              }
         }
-        val jvmMain by getting {
-            dependencies {
-                implementation(libs.kobweb.api)
-             }
-        }
+//        val jvmMain by getting {
+//            dependencies {
+//                implementation(libs.kobweb.api)
+//             }
+//        }
     }
 }
